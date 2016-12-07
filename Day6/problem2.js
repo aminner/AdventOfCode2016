4bb31ec72b8da465d32cea4f9070f6280a0018aa
aminner <amanda.minner@outlook.com>
2016-12-07 10:41:26 -0500

    Day7 Problem 1 working

---
+++ Day6/problem2.js
@@ -0,0 +1,59 @@
+var utils = require("../AOCUtils.js")
+const BASE_PATH = "./Day6/" 
+
+function start()
+{
+    var inputFileName = "input.txt";
+    if(process.argv.length >= 3)
+    {
+      inputFileName = process.argv[2];
+    }
+    var filePath = BASE_PATH + inputFileName;
+    console.log(filePath);
+    utils.read(filePath, { error: function(error){
+       console.log(error); 
+    }, successful: function(inputData){
+        processInput(inputData.trim().split('\n'));
+        }
+    });
+}
+
+function processInput(inputLines)
+{
+    var columnInputs = [];
+    inputLines.forEach(function(line)
+    {
+        line = line.trim();
+        for(var i =0; i< line.length; i++)
+        {
+            if(columnInputs.length < line.length)
+                columnInputs.push({});
+            if(line[i] in columnInputs[i])
+                columnInputs[i][line[i]]++;
+            else
+                columnInputs[i][line[i]] =  1;
+        }
+    });
+    var finalCode = "";
+    for(var i =0; i< columnInputs.length; i++)
+    {
+        var sortedValues = Object.keys(columnInputs[i]).sort(function(a, b)
+        {
+            if(columnInputs[i][a] < columnInputs[i][b])
+                return - 1;
+            if(columnInputs[i][a] > columnInputs[i][b])
+                return 1;
+            return 0;
+        });
+        console.log("Start");
+        for(var j=0; j< sortedValues.length; j++)
+        {
+            console.log(sortedValues[j] + ": "  + columnInputs[i][sortedValues[j]]);
+        }
+        console.log("Final");
+        finalCode += sortedValues[0];
+    }
+    console.log(finalCode);
+}
+
+start();
---
+++ Day7/problem1.js
@@ -0,0 +1,64 @@
+var utils = require("../AOCUtils.js")
+const BASE_PATH = "./Day7/" 
+
+function start()
+{
+    var inputFileName = "input.txt";
+    if(process.argv.length >= 3)
+    {
+      inputFileName = process.argv[2];
+    }
+    var filePath = BASE_PATH + inputFileName;
+    console.log(filePath);
+    utils.read(filePath, { error: function(error){
+       console.log(error); 
+    }, successful: function(inputData){
+        processInput(inputData.trim().split('\n'));
+        }
+    });
+}
+
+function processInput(inputLines)
+{
+    var columnInputs = [];
+    var palindromCount = 0;
+    var regEx = /(\[[\w]+\])/;
+    inputLines.forEach(function(line)
+    {
+        var linePieces = line.trim().split(regEx);
+        var palindromFound = false;
+        var hyperNetPalindromFound = false;
+        
+        linePieces.forEach(function(piece)
+        {
+            var isHypernet = piece.match(regEx) !=null;
+            for(var i=0; i <= piece.length-4; i++)
+            {
+                var subPieceToCheck = piece.substring(i, i+4);
+                var isPalindrom = checkPalindrom(subPieceToCheck);
+                var isAllSameLetter = /^(.)\1+$/.test(subPieceToCheck);
+                if(isPalindrom&&!isAllSameLetter)
+                {
+                    if(isHypernet)
+                        hyperNetPalindromFound = true;
+                    else
+                        palindromFound = true;
+                }
+            }
+            console.log("End \t piece: " + piece + "\tpalindromFound: " + palindromFound + "\thyperNetPalindromFound: " + hyperNetPalindromFound);
+        });
+        if(palindromFound && !hyperNetPalindromFound)
+        {
+            palindromCount++;
+            console.log(line + " - " + palindromFound + " - " + hyperNetPalindromFound + "\n");
+            console.log(palindromCount);
+        }
+    });
+   
+}
+
+function checkPalindrom(str) {
+    return str == str.split('').reverse().join('');
+}
+
+start();
